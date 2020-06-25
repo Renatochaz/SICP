57c6a0aa0218969bb795e359749570120041128b
RenatoChaz <renato.chaves94@hotmail.com>
2020-06-25 11:38:24 -0300

    First class project

---
+++ Projects/project0.scm
@@ -0,0 +1,73 @@
+;;; Example
+
+(define (fib n)
+  ;; calculate the nth fibonnaci number recursively
+  (if (< n 2)
+      1
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+;;; Project 0
+
+;;; Evaluating simples expressions and printing results
+
+-37
+;Value: -37
+
+(* 3 4)
+;Value: 12
+
+(> 10 9.7)
+;Value: #t
+
+(- (if (> 3 4)
+       7
+       10)
+   (/ 16 10))
+;Value: 42/5
+
+(* (- 25 10)
+   (+ 6 3))
+;Value: 135
+
++
+;Value: #[arity-dispatched-procedure 14]
+
+;;; Defining arguments?, testing and printing results
+
+(define double (lambda (x) (* 2 x)))
+;Value: double
+
+double
+;Value: #[compound-procedure 16 double]
+
+(define c 4)
+;Value: c
+
+c
+;Value: 4
+
+(double c)
+;Value: 8
+
+c
+;Value: 4
+
+(double (double (+ c 5)))
+;Value: 36
+
+(define times-2 double)
+;Value: times-2
+
+(times-2 c)
+;Value: 8
+
+(define d c)
+;Value: d
+
+(= c d)
+;Value: #t
+
+(cond ((>= c 2) d)
+      ((= c (- d 5)) (+ c d))
+      (else (abs (- c d))))
+
